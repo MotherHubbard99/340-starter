a39542665ef8efc58ff700b710a89337808f5e9e
angie <angiewheeler99@gmail.com>
2025-09-13 09:52:45 -0600

    Week 02 Task One

---
+++ database/assignment2.sql
@@ -0,0 +1,32 @@
+INSERT INTO account(
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES (
+        'Tony',
+        'Stark',
+        'tony@starkent.com',
+        'Iam1ronM@n'
+    );
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_lastname = 'Stark';
+DELETE FROM account
+WHERE account_lastname = 'Stark';
+SELECT inv_make,
+    inv_model,
+    classification_name
+FROM inventory
+    INNER JOIN classification ON inventory.classification_id = classification.classification_id
+WHERE classification.classification_name = 'Sport';
+UPDATE inventory
+SET inv_description = REPLACE(
+        inv_description,
+        'small interiors',
+        'huge interior'
+    );
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles');
